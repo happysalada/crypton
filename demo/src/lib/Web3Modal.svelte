07e3c2afec46e338c87da8d11ef3ec91fe4453a9
happysalada <raphael@megzari.com>
2022-05-21 05:56:37 -0400

    frontend: add checkout page

--- demo/src/lib/Web3Modal.svelte
+++ demo/src/lib/Web3Modal.svelte
@@ -1,48 +1,165 @@
-<script>
-  import { defaultEvmStores, web3, selectedAccount, walletType, connected, chainId, chainData } from 'svelte-web3'
-  import { Balance } from 'svelte-web3/components'
-  import { goto } from "$app/navigation"
-  import Web3Modal from "web3modal";
-  import { variables } from '$lib/variables';
-  // import CoinbaseWalletSDK from '@coinbase/wallet-sdk'
-  const WalletConnectProvider = window.WalletConnectProvider.default
-  // import WalletConnectProvider from "@walletconnect/web3-provider";
-  const disable = () => defaultEvmStores.disconnect()
-  let web3Modal
-  const enable = async () => {
-    if (web3Modal) web3Modal.clearCachedProvider()
-    web3Modal = new Web3Modal({
-      cacheProvider: false,
-      providerOptions: {
-        walletconnect: {
-          package: WalletConnectProvider,
-          options: { infuraId: variables.infuraId }
-        },
-        // coinbasewallet: {
-        //   package: CoinbaseWalletSDK,
-        //   options: {
-        //     appName: "crypton",
-        //     infuraId: "7b305244a8dc444f9cdd411c1a0b10cf"
-        //   }
-        // },
-      },
-      disableInjectedProvider: false,
-    })
-    const provider = await web3Modal.connect()
-    defaultEvmStores.setProvider(provider)
-  }
-  const login = async () => {
-    console.log('$web3.eth.getChainId', await $web3.eth.getChainId())
-    goto("/dashboard")
-  }
-  $: if ($connected && $web3) login()
+<script lang="ts">
+	import {
+		defaultEvmStores,
+		web3,
+		selectedAccount,
+		walletType,
+		connected,
+		chainId,
+		chainData,
+	} from "svelte-web3";
+	import { Balance } from "svelte-web3/components";
+	import Web3Modal from "web3modal";
+	import { variables } from "$lib/variables";
+	import { chains } from "../../../data/chains";
+	// import CoinbaseWalletSDK from '@coinbase/wallet-sdk'
+	const WalletConnectProvider = window.WalletConnectProvider.default;
+	// import WalletConnectProvider from "@walletconnect/web3-provider";
+	const disable = () => defaultEvmStores.disconnect();
+	let web3Modal;
+	const connectWallet = async () => {
+		if (web3Modal) web3Modal.clearCachedProvider();
+		web3Modal = new Web3Modal({
+			cacheProvider: false,
+			providerOptions: {
+				walletconnect: {
+					package: WalletConnectProvider,
+					options: { infuraId: variables.infuraId },
+				},
+				// coinbasewallet: {
+				//   package: CoinbaseWalletSDK,
+				//   options: {
+				//     appName: "crypton",
+				//     infuraId: "7b305244a8dc444f9cdd411c1a0b10cf"
+				//   }
+				// },
+			},
+			disableInjectedProvider: false,
+		});
+		const provider = await web3Modal.connect();
+		defaultEvmStores.setProvider(provider);
+	};
+	const pay = async () => {
+		console.log("todo");
+	};
+
+	let chainDropdownMenuOpen = false;
+	function toggleChainDropdownMenu() {
+		chainDropdownMenuOpen = !chainDropdownMenuOpen;
+	}
+
+	function getChainId(desiredChainId: number): any {
+		return (
+			chains.filter(({ chainId }) => desiredChainId == chainId)[0] || chains[0]
+		);
+	}
+
+	async function switchNetwork(chainId: number) {
+		if ($chainData.chainId !== chainId) {
+			try {
+				await $web3.eth.currentProvider.request({
+					method: "wallet_switchEthereumChain",
+					params: [{ chainId: $web3.utils.toHex(chainId) }],
+				});
+			} catch (err) {
+				console.log(err);
+				const chain = getChainId(chainId);
+
+				// This error code indicates that the chain has not been added to MetaMask
+				if (err.code === 4902) {
+					await $web3.eth.currentProvider.request({
+						method: "wallet_addEthereumChain",
+						params: [
+							{
+								chainName: chain.name,
+								chainId: $web3.utils.toHex(chainId),
+								nativeCurrency: chain.nativeCurrency,
+								rpcUrls: chain.rpc,
+							},
+						],
+					});
+				}
+			}
+		}
+		chainDropdownMenuOpen = false;
+	}
 </script>
 
 {#if $web3.version}
-  {#if $connected}
-    <button class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50" on:click="{disable}">Logout</button>
-  {:else}
-    <button class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50" on:click="{enable}">Login</button>
-  {/if}
-{/if}
+	{#if $connected}
+		<div
+			class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mb-4"
+		>
+			<!-- Chain dropdown -->
+			<div class="relative">
+				<div class="flex">
+          <p class="my-auto mr-6"> Payment method:</p>
+          <button
+            type="button"
+            class="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						on:click={toggleChainDropdownMenu}
+          >
+            {$chainData?.name} in {$chainData.nativeCurrency.symbol}            
+						<img
+							class="h-8 w-8 rounded-full mx-4"
+							src={getChainId($chainData?.chainId).assetSrc || ""}
+							alt=""
+						/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+          </button>
+				</div>
 
+				<!--
+            Dropdown menu, show/hide based on menu state.
+
+            Entering: "transition ease-out duration-100"
+              From: "transform opacity-0 scale-95"
+              To: "transform opacity-100 scale-100"
+            Leaving: "transition ease-in duration-75"
+              From: "transform opacity-100 scale-100"
+              To: "transform opacity-0 scale-95"
+          -->
+				<div
+					class="{chainDropdownMenuOpen
+						? ''
+						: 'hidden'} origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50"
+					role="menu"
+					aria-orientation="vertical"
+					aria-labelledby="user-menu-button"
+					tabindex="-1"
+				>
+					{#each chains as { assetSrc, name, chainId }}
+						<button
+							class="block px-4 py-2 text-sm text-gray-700"
+							role="menuitem"
+							tabindex="-1"
+							id="user-menu-item-0"
+							on:click|preventDefault={() => switchNetwork(chainId)}
+						>
+							<div class="flex">
+								<img class="h-8 w-8 rounded-full" src={assetSrc || ""} alt="" />
+								<p class="m-auto">{name}</p>
+							</div>
+						</button>
+					{/each}
+				</div>
+			</div>
+		</div>
+
+		<button
+			class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+			on:click|preventDefault={pay}>Pay now</button
+		>
+		<button
+			class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
+			on:click={disable}>Disconnect wallet</button
+		>
+	{:else}
+		<button
+			class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+			on:click|preventDefault={connectWallet}>Connect your wallet</button
+		>
+	{/if}
+{/if}
